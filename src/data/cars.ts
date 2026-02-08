d0110d9d136f389bce1a16ef0087469a9f9f64fd
abdullahfaqot <abdullahabdillahh2002@gmail.com>
2026-02-09 02:23:33 +0700

    Initial commit: Hyundai landing page

---
+++ src/data/cars.ts
@@ -0,0 +1,26 @@
+export const cars = [
+  {
+    name: "Creta Alpha",
+    slug: "creta-alpha",
+    price: "Rp 300 Jutaan",
+    image: "/images/cretan.png",
+    description:
+      "Hyundai Creta Alpha hadir dengan desain sporty, fitur modern, dan kenyamanan maksimal.",
+  },
+  {
+    name: "Stargazer Cartenz X",
+    slug: "stargazer-cartenz-x",
+    price: "Rp 330 Jutaan",
+    image: "/images/cartenz-x.png",
+    description:
+      "MPV premium dengan kabin luas, cocok untuk keluarga modern.",
+  },
+  {
+    name: "Venue",
+    slug: "venue",
+    price: "Rp 280 Jutaan",
+    image: "/images/venue.png",
+    description:
+      "SUV compact yang lincah, stylish, dan irit bahan bakar.",
+  },
+];
